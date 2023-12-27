29635f10ffe844d632071d7e3907bd5e8880dfa9
Matt Hartenbower <matt.hartenbower@gmail.com>
2023-12-27 12:18:43 -0600

    Add goenv to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,7 @@ end
 alias vim="nvim"
 alias k="kubectl"
 
-set --global tide_left_prompt_items pwd character
-set --global tide_right_prompt_items time 
+# GOENV config
+set --global GOENV_ROOT $HOME/.goenv
+fish_add_path $GOENV_ROOT/bin
+eval "$(goenv init -)"
